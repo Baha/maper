c0d41a41c08c5d6d5779977a0262cbf1647fd42c
Baha <ukonat@gmail.com>
2017-11-21 10:03:27 +0100

    Initial commit

---
+++ erl2pl.erl
@@ -0,0 +1,20 @@
+-module(erl2pl).
+-export([main/1]).
+
+main(File) ->
+  case compile:file(File, [to_core, binary, no_copt]) of
+    {ok, ModuleName, CoreForms} ->
+        TransForms = cerl_trees:map(fun translate/1, CoreForms),
+        CoreName = atom_to_list(ModuleName) ++ ".txt",
+        file:write_file(CoreName,
+                        cerl_prettypr:format(TransForms));
+    _ ->
+      io:fwrite("Error: Could not compile file.~n"),
+      exit(file_error)
+  end.
+
+translate(Node) ->
+  case cerl:type(Node) of
+    Node -> Node
+    % cases
+  end.
