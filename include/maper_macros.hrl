f54b94676b3cf90875dd1816cd216003f1915239
Baha <ukonat@gmail.com>
2017-11-22 18:12:47 +0100

    Defines macros for predicates

---
+++ include/maper_macros.hrl
@@ -0,0 +1,100 @@
+-define(ARG_SEP,    ",").
+-define(ARG_START,  "(").
+-define(ARG_END,    ")").
+-define(LIST_SEP,   ",").
+-define(LIST_START, "[").
+-define(LIST_END,   "]").
+
+-define(INT_ATOM,    "int").
+-define(ATOM_ATOM,   "atom").
+-define(PAIR_ATOM,   "pair").
+-define(MODULE_ATOM, "module").
+-define(FUN_ATOM,    "fun").
+-define(LIT_ATOM,    "lit").
+-define(VAR_ATOM,    "var").
+-define(CONS_ATOM,   "cons").
+-define(TUPLE_ATOM,  "tuple").
+-define(VALUES_ATOM, "values").
+-define(LET_ATOM,    "let").
+-define(CASE_ATOM,   "case").
+-define(CLAUSE_ATOM, "clause").
+-define(APPLY_ATOM,  "apply").
+-define(CALL_ATOM,   "call").
+-define(PRIMOP_ATOM, "primop").  
+
+-define(INT_PRED(Int),
+  ?INT_ATOM ++ ?ARG_START ++
+  Int ++ ?ARG_END).
+
+-define(ATOM_PRED(Atom),
+  ?ATOM_ATOM ++ ?ARG_START ++
+  Atom ++ ?ARG_END).
+
+-define(PAIR_PRED(Elem1, Elem2),
+  ?PAIR_ATOM ++ ?ARG_START ++
+  Elem1 ++ ?ARG_SEP ++
+  Elem2 ++ ?ARG_END).
+
+-define(MODULE_PRED(Name, Defs),
+  ?MODULE_ATOM ++ ?ARG_START ++
+  Name ++ ?ARG_SEP ++
+  Defs ++ ?ARG_END).
+
+-define(FUN_PRED(Vars, Body),
+  ?FUN_ATOM ++ ?ARG_START ++
+  Vars ++ ?ARG_SEP ++
+  Body ++ ?ARG_END).
+
+-define(LIT_PRED(Lit),
+  ?LIT_ATOM ++ ?ARG_START ++
+  Lit ++ ?ARG_END).
+
+-define(VAR_PRED(Var),
+  ?VAR_ATOM ++ ?ARG_START ++
+  Var ++ ?ARG_END).
+
+-define(CONS_PRED(Hd, Tl),
+  ?CONS_ATOM ++ ?ARG_START ++
+  Hd ++ ?ARG_SEP ++
+  Tl ++ ?ARG_END).
+
+-define(TUPLE_PRED(Es),
+  ?TUPLE_ATOM ++ ?ARG_START ++
+  Es ++ ?ARG_END).
+
+-define(VALUES_PRED(Es),
+  ?VALUES_ATOM ++ ?ARG_START ++
+  Es ++ ?ARG_END).
+
+-define(LET_PRED(Vars, Arg, Body),
+  ?LET_ATOM ++ ?ARG_START ++
+  Vars ++ ?ARG_SEP ++
+  Arg  ++ ?ARG_SEP ++
+  Body ++ ?ARG_END).
+
+-define(CASE_PRED(Arg, Clauses),
+  ?CASE_ATOM ++ ?ARG_START ++
+  Arg     ++ ?ARG_SEP ++
+  Clauses ++ ?ARG_END).
+
+-define(CLAUSE_PRED(Pats, Guard, Body),
+  ?CLAUSE_ATOM ++ ?ARG_START ++
+  Pats  ++ ?ARG_SEP ++
+  Guard ++ ?ARG_SEP ++
+  Body  ++ ?ARG_END).
+
+-define(APPLY_PRED(Op, Args),
+  ?APPLY_ATOM ++ ?ARG_START ++
+  Op   ++ ?ARG_SEP ++
+  Args ++ ?ARG_END).
+
+-define(CALL_PRED(Module, Name, Args),
+  ?CALL_ATOM ++ ?ARG_START ++
+  Module ++ ?ARG_SEP ++
+  Name   ++ ?ARG_SEP ++
+  Args   ++ ?ARG_END).
+
+-define(PRIMOP_PRED(Op, Args),
+  ?PRIMOP_ATOM ++ ?ARG_START ++
+  Op   ++ ?ARG_SEP ++
+  Args ++ ?ARG_END).
