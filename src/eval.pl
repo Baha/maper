04fdf5fe7950f02dddac96aec12b71009c27f96f
Baha <ukonat@gmail.com>
2017-12-12 17:48:09 +0100

    Adds code for pattern matching literals

--- src/eval.pl
+++ src/eval.pl
@@ -64,9 +64,10 @@ tr(cf(IEnv,let(Vars,IExp1,IExp2)),cf(FEnv,Exp)) :-
 %% (Case)
 tr(cf(IEnv,case(IExp,Clauses)),cf(FEnv,Exp)) :-
   IEnv = (top,_),
-  tr(cf(IEnv,IExp),cf(MEnv,MExp)),
+  format_values(IExp,VExps),
+  tr_list(IEnv,VExps,MEnv,MExps),
   % TODO: Implement match rule
-  match(MEnv,MExp,Clauses,NEnv,NExp),
+  match(MEnv,MExps,Clauses,NEnv,NExp),
   tr(cf(NEnv,NExp),cf(FEnv,FExp)),
   ite(FEnv,FExp,Exp).
 
@@ -98,3 +99,41 @@ ite(Env, _, error) :-
   Env = (bot,_).
 ite(Env,Exp,Exp) :-
   Env = (top,_).
+
+
+format_values(values(Exps),Exps).
+format_values(Exp,[Exp]).
+
+match(IEnv,IExps,_,IEnv,IExps) :-
+  IEnv = (bot,_).
+match(IEnv,IExps,Clauses,JEnv,CExp) :-
+  IEnv = (top,IBinds),
+  match_list(IEnv,IExps,Clauses,CEnv,CExp),
+  CEnv = (Error,CBinds),
+  append(IBinds,CBinds,JBinds),
+  JEnv = (Error,JBinds).
+
+match_list(Env,Exps,[Clause|_],CEnv,CExp) :-
+  match_clause(Env,Exps,Clause,CEnv,CExp,true).
+match_list(Env,Exps,[Clause|Clauses],CEnv,CExp) :-
+  match_clause(Env,Exps,Clause,_,_,false),
+  match_list(Env,Exps,Clauses,CEnv,CExp).
+
+match_clause(IEnv,IExps,clause(Pats,_,Body),CEnv,Body,true) :-
+  match_pats(IEnv,IExps,Pats,CEnv,true).
+  %% CEnv = (top,CBinds),
+  %% match_guard(CBinds,Guard,true).
+%%
+%% TODO: Not assume guards are always true
+%% match_guard(_,_,true).
+
+match_pats(Env,[],[],Env,true).
+match_pats(Env,[Exp|Exps],[Pat|Pats],FEnv,Res) :-
+  match_pat(Env,Exp,Pat,NEnv,true),
+  match_pats(NEnv,Exps,Pats,FEnv,Res).
+
+
+match_pat(Env,lit(X),lit(X),Env,true).
+%% match_pat(Env,Val,var(Var),Env,true) :-
+%%   var_lookup(Var,Env)
+match_pat(Env,_,_,Env,false).
