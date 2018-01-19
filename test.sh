d22b74f843df2d5cd7ff73356c464a770e399290
Baha <ukonat@gmail.com>
2018-01-19 10:44:36 +0100

    Updates test script

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 cd tests
-erl -noshell -pa ../ebin -run erl2fact main sum -s init stop
+erl -noshell -pa ../ebin -run erl2fact main hello -s init stop #> ../src/tmp.pl
+erl -noshell -pa ../ebin -run erl2fact main factorial -s init stop > ../src/tmp.pl
+erl -noshell -pa ../ebin -run erl2fact main sum -s init stop #> ../src/tmp.pl
 erl -noshell -pa ../ebin -run erl2fact main sum_tuple -s init stop
