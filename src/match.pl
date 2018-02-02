9cffd1cc7a523b10ebd256c530c817d8827c8ec9
Adrian Palacios <ukonat@gmail.com>
2018-02-02 15:15:25 +0100

    Documented match rules

--- src/match.pl
+++ src/match.pl
@@ -1,5 +1,8 @@
 :- include('utils').
 
+%% match(init_env,init_exps,clauses,match_env,match_exp)
+%% matches a list of expressions with a list of clauses and
+%% returns the resulting environment and expression (clause body)
 match(IEnv,IExps,_,IEnv,IExps) :-
   IEnv = (bot,_).
 match(IEnv,IExps,Clauses,JEnv,CExp) :-
@@ -15,14 +18,20 @@ match_list(Env,Exps,[Clause|Clauses],CEnv,CExp) :-
   match_clause(Env,Exps,Clause,_,_,false),
   match_list(Env,Exps,Clauses,CEnv,CExp).
 
-%% TODO: Not assume guards are always true
-%% match_guard(_,_,true).
+%% match_clause(init_env,init_exps,clause,match_env,match_exp,result)
+%% matches a list of expressions with a clause and
+%% returns the result (and environment and expression if true)
 match_clause(IEnv,IExps,clause(Pats,_,Body),CEnv,Body,true) :-
   match_pats(IEnv,IExps,Pats,CEnv,true).
+  %% TODO: Not assume guards are always true
+  %% match_guard(_,_,true).
   %% CEnv = (top,CBinds),
   %% match_guard(CBinds,Guard,true).
 match_clause(_,_,_,_,_,false).
 
+%% match_pats(init_env,init_exps,patterns,match_env,result)
+%% matches a list of expressions with a list of patterns and
+%% returns the resulting environment if result is true
 match_pats(Env,[],[],Env,true).
 match_pats(Env,[Exp|Exps],[Pat|Pats],FEnv,Res) :-
   match_pat(Env,Exp,Pat,NEnv,true),
