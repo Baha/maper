89a18a54565fa2e392de712f65f009fde401f616
Emanuele De Angelis <emanuele.de.angelis30@gmail.com>
2018-03-22 10:52:04 +0100

    added unmatchable/3

--- src/match.pl
+++ src/match.pl
@@ -15,9 +15,6 @@ match_list(Env,Exps,[Clause|Clauses],CEnv,CExp) :-
   match_clause(Env,Exps,Clause,_,_,false),           % (**)
   match_list(Env,Exps,Clauses,CEnv,CExp).
 % EMA: general question: What happens CEnv when matching fail?
-% TODO: some code refactoring:
-%   match_clause(Env,Exps,Clause,CEnv,CExp,Res),
-%   ( Res == true ; ( Res == false , match_clause(...) ) )
 
 %% match_clause(init_env,init_exps,clause,match_env,match_exp,result)
 %% matches a list of expressions with a clause and
@@ -69,8 +66,61 @@ match_pat(Env,Val,var(Var),NEnv,true) :-
 %%%% ---------------------------------------------------------------------------
 %match_pat(_,_,_,_,false).           % commented out by Ema.
 match_pat(Env,Exp,Pat,NEnv,false) :- % What happens to NEnv ?
-  Exp \== Pat,
+  unmatchable(Env,Exp,Pat),
   Env = NEnv.                        % Is it equal or not in case of mismatch?
+
+% literals
+unmatchable(_Env,Exp,Pat) :-
+  Pat = lit(Type1,_Val1), Exp = lit(Type2,_Val2), % Pat and Exp are literals,
+  dif(Type1,Type2).                               % of different type
+unmatchable(_Env,Exp,Pat) :-
+  Pat = lit(Type1,Val1), Exp = lit(Type2,Val2), % Pat and Exp are literals,
+  Type1 = Type2,                                % of the same type
+  dif(Val1,Val2).                               % representing different values
+unmatchable(_Env,Exp,Pat) :-
+  Pat =  lit(_,_), Exp \= lit(_,_), % Pat is a lit and Exp is not
+  dif(Exp,Pat).
+% tuples
+unmatchable(_Env,Exp,Pat) :-
+  Pat = tuple(_),  Exp \= tuple(_), % Pat is a tuple and Exp is not
+  dif(Exp,Pat).
+unmatchable(Env,Exp,Pat) :-
+  Pat = tuple(T1), Exp = tuple(T2), % Pat and Exp are both tuples
+  unmatchable_lists(Env,T2,T1),     % and their elements do not match
+  dif(Exp,Pat).
+% lists
+unmatchable(_Env,Exp,Pat) :-
+  Pat = cons(_,_), Exp \= cons(_,_), % Pat is a list and Exp is not
+  dif(Exp,Pat).
+unmatchable(Env,Exp,Pat) :-
+  Pat = cons(_,_), Exp = cons(_,_),  % Pat are both lists
+  cons_to_plList(Exp,Pat,ExpL,PatL),
+  unmatchable_lists(Env,ExpL,PatL).  % and their elements do not match
+% variables
+unmatchable(Env,Exp,Pat) :-
+  Pat = var(Var),
+  Env = (_,Binds),
+  var_lookup(Var,Binds,Val),
+  Val \== undef,
+  dif(Exp,Val).
+
+% NOTE: same representation of lists?
+% unmatchable_lists(env,list(expressions),list(patterns))
+unmatchable_lists(_Env,[_|_],[]).
+unmatchable_lists(_Env,[],[_|_]).
+unmatchable_lists(Env,[E1|_],[E2|_]) :-
+  match_pat(Env,E1,E2,_,false).
+unmatchable_lists(Env,[E1|E1s],[E2|E2s]) :-
+  match_pat(Env,E1,E2,NEnv,true),
+  unmatchable_lists(NEnv,E1s,E2s).
+
+
+% translate lists encoded using cons/2 to Prolog lists
+cons_to_plList(cons(A1,B1),cons(A2,B2),[A1|T1],[A2|T2]) :-
+  !,
+  cons_to_plList(B1,B2,T1,T2).
+cons_to_plList(B1,B2,B1,B2).
+
 
 %% match_guard(env,exp,result)
 %% returns true if guard evaluates to true
@@ -79,4 +129,4 @@ match_guard(Env,Guard,true) :-
 match_guard(Env,Guard,false) :-
   tr(cf(Env,Guard),cf(_,lit(atom,false))).
 match_guard(Env,Guard,false) :-
-  tr(cf(Env,Guard),cf((bot,_),_)).
+  tr(cf(Env,Guard),cf((bot,_),_)). % bad_arg only?
