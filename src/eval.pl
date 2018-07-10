f69493c3c0a84fee86508b945e7ac09f86e8bac8
Emanuele De Angelis <emanuele.deangelis@unich.it>
2018-07-10 23:49:52 +0200

    updated tr for call

--- src/eval.pl
+++ src/eval.pl
@@ -1,37 +1,28 @@
 :- include('match').
 
-:- discontiguous tr/2.
+:- use_module(library(lists)).
+:- use_module(library(terms)).
 
-%% init(mod,fun,args,env,app)
-%% initializes fun (from mod) application
-%% with the corresponding environment
-init(Mod,(Fun,Arity),Args,Env,App) :-
-  retractall(fundef(_,_,_)),
-  consult(Mod),
-  fun_lookup(lit(atom,Mod),var(Fun,Arity),FunDef),
-  FunDef = fun(Pars,_),
-  zip_binds(Pars,Args,Binds),
-  Env = (top,Binds),
-  App = apply(var(Fun,Arity),Pars).
+:- discontiguous tr/2.
 
 %% run(mod,fun,args,final_env,final_exp)
 %% evaluates fun (from mod) application and
 %% returns the final environment and expression
 run(Mod,(Fun,Arity),Args,FEnv,FExp) :-
-  is_list(Args),
-  length(Args,Arity),
+  retractall(fundef(_,_,_)),
+  consult(Mod),
   init(Mod,(Fun,Arity),Args,IEnv,IApp),
   tr(cf(IEnv,IApp),cf(FEnv,FExp)).
-%
-run(_,(_,Arity),Args,[],[]) :-
-  is_list(Args),
-  length(Args,NArgs),
-  Arity \== NArgs.
-%
-run(Mod,(Fun,Arity),Args,FEnv,FExp) :-
-  var(Args),
-  init(Mod,(Fun,Arity),Args,IEnv,IApp),
-  tr(cf(IEnv,IApp),cf(FEnv,FExp)).
+
+%% init(mod,fun,args,env,app)
+%% initializes fun (from mod) application
+%% with the corresponding environment
+init(Mod,(Fun,Arity),Args,Env,App) :-
+  fun_lookup(lit(atom,Mod),var(Fun,Arity),FunDef),
+  FunDef = fun(Pars,_),
+  zip_binds(Pars,Args,Binds),
+  Env = (top,Binds),
+  App = apply(var(Fun,Arity),Pars).
 
 %% tr_list(init_env,init_exps,final_env,final_exps)
 %% evaluates a list of transitions (from exp to exp) and
@@ -74,6 +65,7 @@ tr(cf(IEnv,let(Vars,IExp1,IExp2)),FCf) :-
   IEnv = (top,_),
   tr(cf(IEnv,IExp1),cf(MEnv,FExp1)),
   let_cont(MEnv,let(Vars,FExp1,IExp2), FCf).
+
 % the evaluation of IExp1 succeeds
 let_cont(MEnv,let(Vars,FExp1,IExp2),cf(FEnv,Exp)) :-
   MEnv = (top,MBinds),
@@ -105,32 +97,31 @@ tr(cf(IEnv,apply(FName,IExps)),cf(FEnv3,Exp)) :-
   tr(cf((Error,AppBinds),FunBody),cf((Error2,_),Exp)),
   FEnv3 = (Error2,Binds).
 
-%% (Call1) ---------------------------------------------------------------------
-tr(cf(IEnv,call(Atom,Fname,IExps)),cf(FEnv2,error(badarith))) :-
+%% (Call) ----------------------------------------------------------------------
+tr(cf(IEnv,call(Atom,Fname,IExps)),FCf) :-
   IEnv = (top,_),
   tr_list(IEnv,IExps,FEnv,FExps),
-  types(Atom,Fname,CTypes),
-  types(FExps,ETypes),
-  dif(CTypes,ETypes),
-  FEnv = (_,Binds),
+  types(Atom,Fname,CTypes), % types of Atom (arithmetic or relationa operators)
+  types(FExps,ETypes),      % types of FExps (expressions)
+  % - CTypes is a list of elements in {number,atom} (***)
+  % - ETypes is a list of elements in {int,float,atom}
+  call_cont(Atom,Fname,CTypes,FEnv,FExps,ETypes,FCf).
+
+% (Call1 - arithmetic error)
+call_cont(_Atom,_Fname,CTypes,FEnv1,_FExps,ETypes,cf(FEnv2,error(badarith))) :-
+  diftypes(ETypes,CTypes),
+  FEnv1 = (_St,Binds),
   FEnv2 = (bot,Binds).
+% (Call2 - execute bif)
+call_cont(Atom,Fname,CTypes,FEnv,FExps,ETypes,cf(FEnv,Exp)) :-
+  subtypes(ETypes,CTypes), % see notes above (***)
+  call_cont_bif(Atom,Fname,FExps,Exp).
 
-%% (Call2)
-tr(cf(IEnv,call(Atom,Fname,IExps)),cf(FEnv,error(bad_arg))) :-
-  IEnv = (top,_),
-  tr_list(IEnv,IExps,FEnv,FExps),
-  types(Atom,Fname,CTypes),
-  types(FExps,ETypes),
-  CTypes = ETypes,
+% (Call2.1 - bif terminates erroneously)
+call_cont_bif(Atom,Fname,FExps,error(bad_arg)) :-
   bif(Atom,Fname,FExps,bad_arg).
-
-%% (Call3)
-tr(cf(IEnv,call(Atom,Fname,IExps)),cf(FEnv,Exp)) :-
-  IEnv = (top,_),
-  tr_list(IEnv,IExps,FEnv,FExps),
-  types(Atom,Fname,CTypes),
-  types(FExps,ETypes),
-  CTypes = ETypes,
+% (Call2.2 - bif terminates correctly)
+call_cont_bif(Atom,Fname,FExps,Exp) :-
   bif(Atom,Fname,FExps,Exp).
 
 %% (Primop) --------------------------------------------------------------------
