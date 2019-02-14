15b16e950dd29989493605e7d1c9388ef78e2ca6
Adrian Palacios <ukonat@gmail.com>
2019-02-14 18:10:52 +0100

    Get type info from prop funs

--- src/testing.erl
+++ src/testing.erl
@@ -3,18 +3,22 @@
 
 parse_input(Input, PropFun) ->
   % check if PropFun expects composite
-  PropFormat = lists:nth(2, PropFun),
+  PropClauses = erl_syntax:function_clauses(PropFun),
+  ClauseBody = erl_syntax:clause_body(hd(PropClauses)),
+  {call,_,{remote,_,{atom,_,ProperMod},{atom,_,ProperCall}},CallArgs} = hd(ClauseBody),
+  PropFormat = lists:nth(1, CallArgs),
+  io:format("~p~n", [CallArgs]),
   PInput =
     case is_composite(PropFormat) of
       true ->
         convert_input(Input);
       false ->
-        ok
+        Input
     end,
   PInput.
 
-is_composite(Format) when is_tuple(Format) -> true;
-is_composite(Format) when is_list(Format) -> true;
+is_composite({cons,_,_,_}) -> true;
+is_composite({tuple,_,_})  -> true;
 is_composite(_) -> false.
 
 convert_input(X) -> X.
