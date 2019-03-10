3c777e36e8245c8fe46f58f57f2eed883fc0350c
Baha <ukonat@gmail.com>
2019-03-10 17:19:33 +0100

    typeof preds for user-defined types (needs testing)

--- src/symgen.erl
+++ src/symgen.erl
@@ -4,7 +4,7 @@
 main(File) ->
   Forms = forms:read(File ++ ".erl"),
   UDTypes = forms:filter(fun is_type/1, Forms),
-    io:format("UDTYPES:~n~p~n", [UDTypes]),
+  % io:format("UDTYPES:~n~p~n", [UDTypes]),
   [generate_type(T) || T <- UDTypes],
   PropFuns = forms:filter(fun is_prop_fun/1, Forms),
   [generate_clauses(Fun) || Fun <- PropFuns].
@@ -27,7 +27,7 @@ is_type(_) -> false.
 %% Access can be type (public) or opaque (private)
 generate_type({attribute,_,_Access,TypeDef}) ->
   GenDef = generate_typedef(TypeDef),
-  io:format("~s~n", [GenDef]).
+  io:format("~s~n~n", [GenDef]).
 
 generate_typedef({Name,Def,Args}) ->
 
@@ -46,10 +46,10 @@ generate_type_clauses({type,_,union,Defs},Name) ->
   ClausesStr = [generate_type_clauses(D,Name) || D <- Defs],
   string:join(ClausesStr, "\n");
 generate_type_clauses(X, Name) ->
-  Name ++ pp_type(X).
+  "typeof(" ++ Name ++ "," ++ pp_type(X) ++ ").".
 pp_type({type,_,tuple,TupleEs}) ->
   EsStr = [pp_type(E) || E <- TupleEs],
-  "tuple(" ++ string:join(EsStr, ",")++ ")";
+  "tuple([" ++ string:join(EsStr, ",")++ "])";
 pp_type({user_type,_,Name,Args}) ->
   TName = atom_to_list(Name),
   AName =
@@ -60,8 +60,18 @@ pp_type({user_type,_,Name,Args}) ->
         "(" ++ string:join(ArgsStr, ",") ++ ")"
     end,
   TName ++ AName;
+pp_type({type,_,Name,Args}) ->
+  TName = atom_to_list(Name),
+  AName =
+    case length(Args) of
+      0 -> "";
+      _ ->
+        ArgsStr = [pp_type(A) || A <- Args],
+        "(" ++ string:join(ArgsStr, ",") ++ ")"
+    end,
+  TName ++ AName;
 pp_type({var,_,Var}) -> atom_to_list(Var);
-pp_type({atom,_,Atom}) -> atom_to_list(Atom).
+pp_type({atom,_,Atom}) -> "lit(atom," ++ atom_to_list(Atom) ++ ")".
 
 generate_clauses(Fun) ->
   {function,_,_,0,Clauses} = Fun,
