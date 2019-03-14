aab958135d02ef156d756da59c0afde64b4c42ec
Adrian Palacios <ukonat@gmail.com>
2019-03-14 17:06:31 +0100

    Avoid copying proper_types info to target file

--- test.sh
+++ test.sh
@@ -8,9 +8,7 @@ for test in ord_insert_ok biggest_bug biggest_ok delete_bug mytrees sorted_ok st
   enc_apply enc_list enc_seq enc_tuple \
   factorial foo hello odd sum sum_tuple sum_list sign weird_fun; do
      echo "Translating $test"
-     cat $test_path"proper_types.pl" > $test".pl"
-     echo >> $test".pl"
-     $cmd_init$test$cmd_end >> $test".pl"
+     $cmd_init$test$cmd_end > $test".pl"
      $cmd_init2$test$cmd_end >> $test".pl"
 
 done
